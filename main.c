6d0d13b36d70913f64cc5cc9343e533bd0a779d8
CardinalCandle <youssef-kj@outlook.fr>
2023-04-21 18:07:52 +0000

    Task

--- main.c
+++ main.c
@@ -39,8 +39,6 @@ int main(void)
 	_printf("Len:[%i]\n", len);
 	printf("Len:[%i]\n", len2);
 	_printf("Unknown:[%r]\n");
-	/*
-	printf("Unknown:[%r]\n");
-	*/
+	printf("Unknown:[%%r]\n");
 	return (0);
 }
