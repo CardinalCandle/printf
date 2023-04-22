d9ca17ba30895dfd1e22630f9a8815c599b4d291
CardinalCandle <youssef-kj@outlook.fr>
2023-04-22 23:03:35 +0000

    Task

--- _putchar.c
+++ _putchar.c
@@ -1,12 +1,10 @@
-#include "main.h"
-
+#include <unistd.h>
 /**
- * _putchar - prints/writes character
- * @character: The character to print
- * Return: 1, success, -1 error
+ * _putchar - writes the character c to stdout
+ * @c: the character to print
+ * Return: 0 on success and -1 error and errno is set appropiately
  */
-
-int _putchar(char character)
+int _putchar(char c)
 {
-	return (write(1, &character, 1));
+	return (write(1, &c, 1));
 }
