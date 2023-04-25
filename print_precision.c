675baff3ddbbaa5951dbbdc73e59fa1c814198c7
CardinalCandle <youssef-kj@outlook.fr>
2023-04-25 22:23:46 +0000

    Task

--- print_precision.c
+++ print_precision.c
@@ -39,3 +39,63 @@ int get_precision(const char *format, int *i, va_list list)
 
 	return (precision);
 }
+
+/**
+ * get_size - Calculates the size to cast the argument
+ * @format: Formatted string in which to print the arguments
+ * @i: List of arguments to be printed.
+ *
+ * Return: Precision.
+ */
+int get_size(const char *format, int *i)
+{
+	int curr_i = *i + 1;
+	int s = 0;
+
+	if (format[curr_i] == 'l')
+		s = S_LONG;
+	else if (format[curr_i] == 'h')
+		s = S_SHORT;
+
+	if (s == 0)
+		*i = curr_i - 1;
+	else
+		*i = curr_i;
+
+	return (s);
+}
+
+/**
+ * get_width - Calculates the width for printing
+ * @format: Formatted string in which to print the arguments.
+ * @i: List of arguments to be printed.
+ * @list: list of arguments.
+ *
+ * Return: width.
+ */
+int get_width(const char *format, int *i, va_list list)
+{
+	int curr_i;
+	int w = 0;
+
+	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
+	{
+		if (is_digit(format[curr_i]))
+		{
+			w *= 10;
+			w += format[curr_i] - '0';
+		}
+		else if (format[curr_i] == '*')
+		{
+			curr_i++;
+			w = va_arg(list, int);
+			break;
+		}
+		else
+		break;
+	}
+
+	*i = curr_i - 1;
+
+	return (w);
+}
